34e7fad57b409a2fc120e1a33ce7623913360f73
Daniel Cook <danielsteven.cook@gmail.com>
2019-05-22 19:56:40 +0100

    Added naive bayes baseline.

--- news-classification/code/evaluation.py
+++ news-classification/code/evaluation.py
@@ -25,8 +25,60 @@ def calculate_classification_accuracy(int_to_topic_code, predicted, actual):
     return per_class_accuracy
 
 
+def macro_f1_score(int_to_topic_code, predicted, actual):
+    """
+    Calculates the macro F1 score.
+        
+    :param int_to_topic_code: the index to topic lookup.
+    :param predicted: the predicted class labels.
+    :param actual: the actual class labels.
+    :returns: the macro F1 score.
+    """
+    sum_f1_score = 0
+    num_f1_scores = 0
+    
+    for index in int_to_topic_code.keys():
+        filtered_predicted = predicted[actual == index]
+        
+        tp = (filtered_predicted == index).sum()
+        tp_and_fp = (predicted == index).sum()
+        tp_and_fn = (actual == index).sum()
+                
+        precision = tp / tp_and_fp
+        recall = tp / tp_and_fn
+                
+        sum_f1_score += (2 * precision * recall) / (precision + recall)
+        num_f1_scores += 1
+        
+    return sum_f1_score / num_f1_scores
+    
 
-#per_class_accuracy = calculate_classification_accuracy({0: 'A', 1: 'B', 2: 'C'}, 
-#                                                       np.array([0, 1, 0, 1, 1]), 
-#                                                       np.array([0, 0, 0, 1, 2]))
-#print(per_class_accuracy)
+
+def micro_f1_score(int_to_topic_code, predicted, actual):
+    """
+    Calculates the micro F1 score
+        
+    :param int_to_topic_code: the index to topic lookup.
+    :param predicted: the predicted class labels.
+    :param actual: the actual class labels.
+    :returns: the micro F1 score.
+    """
+    sum_tp = 0
+    sum_tp_and_fp = 0
+    sum_tp_and_fn = 0
+    
+    for index in int_to_topic_code.keys():
+        filtered_predicted = predicted[actual == index]
+        
+        tp = (filtered_predicted == index).sum()
+        tp_and_fp = (predicted == index).sum()
+        tp_and_fn = (actual == index).sum()
+        
+        sum_tp += tp
+        sum_tp_and_fp += tp_and_fp
+        sum_tp_and_fn += tp_and_fn
+    
+    precision = sum_tp / sum_tp_and_fp
+    recall = sum_tp / sum_tp_and_fn
+    
+    return (2 * precision * recall) / (precision + recall)
