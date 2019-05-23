562501e7b78e4c402d23c0e1f708718cbc628f40
Daniel Cook <danielsteven.cook@gmail.com>
2019-05-23 11:37:11 +0100

    Code restructure.

---
+++ tweet-classification/tweet_parser.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+import os
+import json
+
+def load_data(tweet_limit, directory):
+    """
+    Load the tweet data, limiting to the given number of tweets per topic.
+    Accepts only English language tweets and rejects duplicates such as retweets.
+    
+    :param tweet_limit: the maximum number of tweets per topic.
+    :param directory: the directory where the tweets are located.
+    :returns: a dictionary of tweets keyed by topic.
+    """
+    tweets_keyed_by_topic = defaultdict(list)
+    
+    # Obtain the list of topics
+    tweet_topics = os.listdir(directory)
+
+    # Load the tweets for each topic
+    for topic in tweet_topics:
+        tweets = []
+        
+        topic_name = topic[:-6]
+        topic_path = directory + topic
+                
+        # Process tweets until limit reached
+        count = 0
+        with open(topic_path) as file:
+            for line in file:
+                tweet = json.loads(line)
+                
+                # Accept only english language tweets and non retweets
+                if tweet['lang'] == 'en' and not 'retweeted_status' in tweet:
+                    tweets.append(tweet['full_text'])
+                    count += 1
+                    if count == tweet_limit:
+                        break
+        
+        # Add tweets to dictionary
+        tweets_keyed_by_topic[topic_name] = tweets
+    
+    
+    return tweets_keyed_by_topic
+
+
+tweets_keyed_by_topic = load_data(10, 'data/')
+
+for topic, tweets in tweets_keyed_by_topic.items():
+    print('Topic: {}'.format(topic))
+    print('Tweets: {}'.format(tweets))
+    print('-----------------------')
