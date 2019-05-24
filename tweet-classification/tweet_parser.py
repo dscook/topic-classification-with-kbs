ee907b2c0ec50b22cb5f66b145f2c072772584f8
Daniel Cook <danielsteven.cook@gmail.com>
2019-05-24 12:54:20 +0100

    First draft of tweet parsing functionality.

--- tweet-classification/tweet_parser.py
+++ tweet-classification/tweet_parser.py
@@ -3,6 +3,8 @@
 from collections import defaultdict
 import os
 import json
+import re
+
 
 def load_data(tweet_limit, directory):
     """
@@ -17,7 +19,7 @@ def load_data(tweet_limit, directory):
     
     # Obtain the list of topics
     tweet_topics = os.listdir(directory)
-
+    
     # Load the tweets for each topic
     for topic in tweet_topics:
         tweets = []
@@ -33,19 +35,16 @@ def load_data(tweet_limit, directory):
                 
                 # Extract the tweet
                 tweet_text = tweet['full_text']
-                
-                print(tweet_text)
-                print('----')
-                
+                                
                 # Replace mentions with names of people/organisations
                 prev_upper_index = 0
                 expanded_tweet_text = ''
                 
                 for user_mention in tweet['entities']['user_mentions']:
-                    lower_index = user_mention['indices'][0] + 1
+                    lower_index = user_mention['indices'][0]
                     expanded_tweet_text += tweet_text[prev_upper_index:lower_index]
                     expanded_tweet_text += user_mention['name']
-                    prev_upper_index = user_mention['indices'][1] + 1
+                    prev_upper_index = user_mention['indices'][1]
                 
                 expanded_tweet_text += tweet_text[prev_upper_index:]
                 
@@ -61,12 +60,92 @@ def load_data(tweet_limit, directory):
     return tweets_keyed_by_topic
 
 
-tweets_keyed_by_topic = load_data(10, 'data/')
+def create_topic_hashtags_dict(directory):
+    """
+    Create a dictionary of topic names to the list of hashtags that were used to find tweets from that topic.
+    
+    :param: directory: the path of a directory containing a file per topic, with each file containing
+                       a hashtag per line.
+    :returns: a dictionary of hashtag lists keyed by topic.
+    """
+    topic_hashtags_dict = {}
+    
+    hashtag_lists = os.listdir(directory)
+    
+    for hashtag_list in hashtag_lists:
+        hashtag_list_path = directory + hashtag_list
+        with open(hashtag_list_path) as file:
+            hashtags = []
+            for line in file:
+                hashtags.append(line.strip())
+        topic_hashtags_dict[hashtag_list] = hashtags
+    
+    return topic_hashtags_dict
+            
 
-for topic, tweets in tweets_keyed_by_topic.items():
-    print('Topic: {}'.format(topic))
-    print('')
-    for tweet in tweets:
-        print('{}'.format(tweet))
-        print('-----------------------')
-    print('-----------------------')
+
+def cleanup_tweets(tweets_keyed_by_topic, topic_hashtags_dict):
+    """
+    Given a dictionary of lists of tweets keyed by topic, cleans up the tweet text as follows:
+        * Removed hashtags that were used to find the tweets as these are topic tags.
+        * Remove any URLs present in the tweet as these add no value unless they are resolved and summarised.
+        * Remove any sequence of hashtags at the end of the tweet as these are effectively topic tags.
+        * For remaining hashtags, remove hash and split into words when camel case or separated by underscores.
+        
+    :param: tweets_keyed_by_topic: dictionary of topics to their list of tweets.
+    :param: topic_hashtags_dict: a dictionary of hashtag lists keyed by topic.
+    :returns: cleaned up version of tweets_keyed_by_topic.
+    """
+    cleaned_tweets_keyed_by_topic = {}
+    
+    # To match URLs in tweets
+    url_matcher = re.compile(r'(www|http:|https:)[^\s]+')
+    
+    # To match camel case words
+    camel_matcher = re.compile(r'(?<![A-Z])([A-Z][a-z]+)')
+    
+    for topic, tweets in tweets_keyed_by_topic.items():
+        
+        # Process each tweet separately
+        processed_tweets = []
+        for tweet in tweets:
+            
+            # Remove hashtags used to find tweets, these are topic indicators
+            hashtags_to_remove = topic_hashtags_dict[topic]
+            for hashtag in hashtags_to_remove:
+                tweet = tweet.replace(hashtag, '')
+            
+            # Remove URLs present in the tweet
+            tweet = url_matcher.sub('', tweet)
+            
+            # Remove hashtags at end of the tweet as these are effectively topic tags
+            tweet_split = tweet.split()
+            keep_until = len(tweet_split)
+            for i in range(len(tweet_split)-1, -1, -1):
+                if not tweet_split[i].startswith('#'):
+                    break
+                keep_until = i
+                
+            tweet_split = tweet_split[:keep_until]
+            
+            # For remaining hashtags, remove hash and split into words when camel case
+            # or separated by underscores.
+            for i in range(len(tweet_split)):
+                if tweet_split[i].startswith('#'):
+                    tweet_split[i] = tweet_split[i][1:]
+                    
+                    # Check for snake case
+                    if len(tweet_split[i].split('_')) > 1:
+                        tweet_split = tweet_split[:i] + tweet_split[i].split('_') + tweet_split[i+1:]
+                    else:    # Check for camel case
+                        tweet_split = (tweet_split[:i] + 
+                                       camel_matcher.sub(r' \1', tweet_split[i]).split() + 
+                                       tweet_split[i+1:])
+            
+            # Reform tweet string now processing is complete
+            tweet = ' '.join(tweet_split)
+            processed_tweets.append(tweet)
+        
+        cleaned_tweets_keyed_by_topic[topic] = processed_tweets
+    
+    return cleaned_tweets_keyed_by_topic
