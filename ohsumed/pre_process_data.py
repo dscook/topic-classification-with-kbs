a9b937f108dd9f5bcf41bf113ee2498edd89354d
Daniel Cook <danielsteven.cook@gmail.com>
2019-06-20 14:04:40 +0100

    Added code to remove duplicates from lemmatised data.

--- ohsumed/pre_process_data.py
+++ ohsumed/pre_process_data.py
@@ -7,9 +7,14 @@ sys.path.append('../common/')
 
 import numpy as np
 import csv
+import uuid
 
 from ohsumed_parser import load_data
 from sentence_utils import remove_stop_words_and_lemmatize
+
+
+# Used for eliminating duplicate articles
+namespace = uuid.uuid4()
 
 
 def shuffle_data(x, y):
@@ -26,15 +31,29 @@ def shuffle_data(x, y):
     return x, y
 
 
-def remove_empty_examples(x, y):
+# Intentionally here so duplicates that span the train and test set are removed
+seen_so_far = set()
+
+def remove_empty_examples_and_duplicates(x, y):
+    
+    number_of_duplicates = 0
     
     filtered_x = []
     filtered_y = []
     
     for i in range(len(x)):
         if x[i]:
-            filtered_x.append(x[i])
-            filtered_y.append(y[i])
+            
+            article_id = uuid.uuid3(namespace, x[i])
+                        
+            if article_id not in seen_so_far:
+                filtered_x.append(x[i])
+                filtered_y.append(y[i])
+                seen_so_far.add(article_id)
+            else:
+                number_of_duplicates += 1
+        
+    print('{} duplicates removed'.format(number_of_duplicates))
     
     return filtered_x, filtered_y
 
@@ -53,9 +72,9 @@ train_x, train_y, test_x, test_y = load_data('../../../downloads/UVigoMED/single
 train_x = list(map(remove_stop_words_and_lemmatize, train_x))
 test_x = list(map(remove_stop_words_and_lemmatize, test_x))
 
-# Remove any examples that are empty
-train_x, train_y = remove_empty_examples(train_x, train_y)
-test_x, test_y = remove_empty_examples(test_x, test_y)
+# Remove any examples that are empty or duplicates
+train_x, train_y = remove_empty_examples_and_duplicates(train_x, train_y)
+test_x, test_y = remove_empty_examples_and_duplicates(test_x, test_y)
 
 # To ensure the output is always in the same order
 np.random.seed(42)
@@ -65,5 +84,5 @@ train_x, train_y = shuffle_data(train_x, train_y)
 test_x, test_y = shuffle_data(test_x, test_y)
 
 # Write out the data
-write_data(train_x, train_y, 'train-1')
-write_data(test_x, test_y, 'test-1')
+write_data(train_x, train_y, 'train')
+write_data(test_x, test_y, 'test')
