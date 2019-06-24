13e9a8c682d41b854a89532235dbe4a57ec00087
Daniel Cook <danielsteven.cook@gmail.com>
2019-06-24 12:02:47 +0100

    Added script to cleanup medline data.

---
+++ kb-classifier/phrase_generation/cleanup_medline.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import re
+import csv
+
+literal_matcher = re.compile(r'^<.*>\s<http://id\.nlm\.nih\.gov/mesh/vocab#(prefLabel|altLabel)>\s"(.*)"@en \.$')
+
+resources = set()
+with open ('/Users/danielcook/Development/university/Project/downloads/MeSH/mesh2019.nt', 'r') as medline_triples:
+    for line in medline_triples:
+        match  = literal_matcher.match(line)
+        if match:
+            resources.add(match.group(2))
+
+# Write out all resources encountered
+with open('../data-medline/resources.csv', 'w') as resources_csv:
+    resource_writer = csv.writer(resources_csv)
+    for resource in resources:
+        resource_writer.writerow([resource.lower(), resource])
