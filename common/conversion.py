511dbbf2b59b0dfe6ba5b912d9cdcfc5f713d622
Daniel Cook <danielsteven.cook@gmail.com>
2019-07-04 10:08:49 +0100

    Unused NN code removed.
    Doc added to conversion and loader utilities.

--- common/conversion.py
+++ common/conversion.py
@@ -7,6 +7,10 @@ def convert_dictionary_to_array(dataset, topic_code_to_int):
     Given a dataset dictionary keyed by topic code with items a list of the articles for that topic; 
     returns a tuple (x_data, y_data) where x_data is an array of all articles and y_data is the corresponding
     topic indexes from topic_code_to_int.  The data is also shuffled.
+    
+    :param dataset: dictionary keyed by topic code with items a list of the articles for that topic.
+    :param topic_code_to_int: dictionary of topic codes to their integer indexes.
+    :returns: (array of all articles, array of topic indexes)
     """
     x_data = []
     y_data = []
@@ -30,6 +34,11 @@ def convert_dictionary_to_array(dataset, topic_code_to_int):
 
 def convert_array_to_dictionary(x, y, int_to_topic_code):
     """
+    Convert data in array format to a dictionary keyed by topic code.
+    
+    :param x: the array of articles.
+    :param y: the integer indexes of the topic classes.
+    :param int_to_topic_code: dictionary of integer indexes to topic codes.
     :returns: a dictionary keyed by topic code with items a list of the articles for that topic.
     """
     dictionary = {}
--- common/loader.py
+++ common/loader.py
@@ -3,6 +3,13 @@
 import csv
 
 def load_preprocessed_data(path):
+    """
+    Loads already lemmatised/lowercased/stopwords removed data.
+    Data is expected to be in CSV format, each line like: "<TOPIC CLASS INDEX>,<ARTICLE>"
+    
+    :param path: the path to the data to load.
+    :returns: (array of articles, array of the article topic class indexes).
+    """
     y = []
     x = []
 
