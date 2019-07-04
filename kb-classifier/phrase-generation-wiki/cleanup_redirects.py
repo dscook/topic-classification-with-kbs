c43ad600a9dcd89c5dd5a1e3a140b7aa43c9a5a2
Daniel Cook <danielsteven.cook@gmail.com>
2019-07-04 13:49:14 +0100

    Directory shuffle.

--- kb-classifier/phrase-generation-wiki/cleanup_redirects.py
+++ kb-classifier/phrase-generation-wiki/cleanup_redirects.py
@@ -4,17 +4,16 @@ import re
 
 literal_matcher = re.compile(r'^<http://dbpedia.org/resource/(.*)>\s<.*>\s<.*>')
 
-# Because we are processing the categories file and a resource can have many categories,
-# maintain a set of all resources encountered.
 resources = set()
-with open ('/Users/danielcook/Development/university/Project/downloads/dbpedia-2016-10/article_categories_en.ttl', 'r') as resource_categories:
-    for line in resource_categories:
+with open ('/Users/danielcook/Development/university/Project/downloads/dbpedia-2016-10/redirects_en.ttl', 'r') as redirect_resources:
+    for line in redirect_resources:
         match  = literal_matcher.match(line)
-        if match:
+        # Ensure hierarchical URLs are not processed
+        if match and '/' not in match.group(1):
             underscores_removed = ' '.join(match.group(1).split('_'))
             resources.add(underscores_removed)
 
 # Write out all resources encountered
-with open('../data/resources.txt', 'w') as sanitised_resources:
+with open('../data-wiki/redirect_resources.txt', 'w') as sanitised_resources:
     for resource in resources:
         sanitised_resources.write(resource + '\n')
--- kb-classifier/phrase-generation-wiki/cleanup_resources.py
+++ kb-classifier/phrase-generation-wiki/cleanup_resources.py
@@ -4,16 +4,17 @@ import re
 
 literal_matcher = re.compile(r'^<http://dbpedia.org/resource/(.*)>\s<.*>\s<.*>')
 
+# Because we are processing the categories file and a resource can have many categories,
+# maintain a set of all resources encountered.
 resources = set()
-with open ('/Users/danielcook/Development/university/Project/downloads/dbpedia-2016-10/redirects_en.ttl', 'r') as redirect_resources:
-    for line in redirect_resources:
+with open ('/Users/danielcook/Development/university/Project/downloads/dbpedia-2016-10/article_categories_en.ttl', 'r') as resource_categories:
+    for line in resource_categories:
         match  = literal_matcher.match(line)
-        # Ensure hierarchical URLs are not processed
-        if match and '/' not in match.group(1):
+        if match:
             underscores_removed = ' '.join(match.group(1).split('_'))
             resources.add(underscores_removed)
 
 # Write out all resources encountered
-with open('../data/redirect_resources.txt', 'w') as sanitised_resources:
+with open('../data-wiki/resources.txt', 'w') as sanitised_resources:
     for resource in resources:
         sanitised_resources.write(resource + '\n')
