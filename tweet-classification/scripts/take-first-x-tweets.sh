ab77f10dd2d9a146e7acb3ef9af3743174405657
Daniel Cook <danielsteven.cook@gmail.com>
2019-07-04 08:41:52 +0100

    Removed tweet classification directory as no longer used.

--- tweet-classification/scripts/take-first-x-tweets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-head -100000 ../../../../downloads/tweets/brexit/tweets.jsonl > ../data/brexit.jsonl
-head -100000 ../../../../downloads/tweets/climate_change/tweets.jsonl > ../data/climate_change.jsonl
-head -100000 ../../../../downloads/tweets/fake_news/tweets.jsonl > ../data/fake_news.jsonl
-head -100000 ../../../../downloads/tweets/gaza/tweets.jsonl > ../data/gaza.jsonl
-head -100000 ../../../../downloads/tweets/hurricane_harvey/tweets.jsonl > ../data/hurricane_harvey.jsonl
-head -100000 ../../../../downloads/tweets/winter_olympics/tweets.jsonl > ../data/winter_olympics.jsonl
