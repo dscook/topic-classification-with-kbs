c5125243c19a2196e87085190634ca36103bcd9d
Daniel Cook <danielsteven.cook@gmail.com>
2019-07-04 16:26:01 +0100

    Added comments to lookup caches.

--- kb-classifier/lookup_cache_mesh.py
+++ kb-classifier/lookup_cache_mesh.py
@@ -4,9 +4,15 @@ import csv
 
 
 class LookupCache:
+    """
+    Prime a cache of all valid knowledge base phrases to prevent costly DB lookups for phrases that do not exist.
+    """
 
-    
     def __init__(self, resource_path='../kb-classifier/data-mesh/resources.csv'):
+        """
+        :param resource_path: the path to the CSV file containing 
+                              "<case insensitive resource name>, <resource name>" pairs.
+        """
         
         # The resources in the MeSH database, specifically terms.
         # Dictionary keyed by lowercase version of the term to the case exact version of the term.
@@ -19,16 +25,34 @@ class LookupCache:
 
 
     def contains_exact(self, phrase): 
+        """
+        Does an exact resource match for the phrase exist.
+        
+        :param phrase: the phrase to match.
+        :returns: True if a resource exists for the phrase.
+        """
         return phrase in self.resources
     
     
     def contains_redirect(self, phrase):
+        """
+        Redirects do not exist in MeSH.
+        """
         return False
     
     
     def contains_anchor(self, phrase):
+        """
+        Anchors do not exist in MeSH.
+        """
         return False
     
     
     def translate(self, phrase):
+        """
+        Convert the lowercased version of the phrase to its exact case version.
+        
+        :param phrase: lowercase version of the phrase.
+        :returns: case exact version of the phrase.
+        """
         return self.resources[phrase]
--- kb-classifier/lookup_cache_wiki.py
+++ kb-classifier/lookup_cache_wiki.py
@@ -2,13 +2,22 @@
 # -*- coding: utf-8 -*-
 
 class LookupCache:
+    """
+    Prime a cache of all valid knowledge base phrases to prevent costly DB lookups for phrases that do not exist.
+    """
     
     def __init__(self,
                  resource_path='../kb-classifier/data-wiki/resources.txt',
                  redirect_path='../kb-classifier/data-wiki/redirect_resources.txt',
                  anchor_path='../kb-classifier/data-wiki/anchors.txt',
                  use_anchors_only=False):
-        # Prime a cache of all valid phrases to prevent costly DB lookups
+        """
+        :param resource_path: the path to the file containing valid resources in DBpedia.
+        :param redirect_path: the path to the file containing redirects in DBpedia.
+        :param anchor_path: the path to the file containing valid anchor text in DBpedia.
+        :param use_anchors_only: set to True if only anchors should be considered and not direct resource or redirect
+                                 matches.
+        """
         self.resource_cache = self.load_phrase_cache(resource_path)
         self.redirect_cache = self.load_phrase_cache(redirect_path)
         self.anchor_cache = self.load_phrase_cache(anchor_path)
@@ -19,6 +28,9 @@ class LookupCache:
     def load_phrase_cache(self, phrase_path):
         """
         Given a file of phrases,  returns a set of those phrases so it can be used as a cache.
+        
+        :param phrase_path: the path to the file containing phrases.
+        :returns: a set of phrases contained in the file.
         """
         valid_phrases = set()
         with open(phrase_path, 'r') as phrases:
@@ -28,7 +40,13 @@ class LookupCache:
         return valid_phrases
     
     
-    def contains_exact(self, phrase):        
+    def contains_exact(self, phrase):
+        """
+        Does an exact resource match for the phrase exist.
+        
+        :param phrase: the phrase to match.
+        :returns: True if a resource exists for the phrase.  Will always return False if use_anchors_only=True.
+        """
         # Resources always have their first letter as a capital
         for_resource_search = phrase
         if len(for_resource_search) > 1:
@@ -39,6 +57,12 @@ class LookupCache:
     
     
     def contains_redirect(self, phrase):
+        """
+        Does a redirect match for the phrase exist.
+        
+        :param phrase: the phrase to match.
+        :returns: True if a redirect exists for the phrase.  Will always return False if use_anchors_only=True.
+        """
         # Resources always have their first letter as a capital
         for_resource_search = phrase
         if len(for_resource_search) > 1:
@@ -48,9 +72,18 @@ class LookupCache:
     
     
     def contains_anchor(self, phrase):
+        """
+        Does an anchor match for the phrase exist.
+        
+        :param phrase: the phrase to match.
+        :returns: True if an anchor exists for the phrase.
+        """
         return phrase in self.anchor_cache 
 
 
     def translate(self, phrase):
+        """
+        No translation necessary for DBpedia.
+        """
         return phrase
         
