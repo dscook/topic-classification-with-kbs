2e634316ccc8460847b1c68d4f0a78240c0217c7
Daniel Cook <danielsteven.cook@gmail.com>
2019-07-05 10:03:53 +0100

    Fixed HTTP REST layer.

--- kb-classifier/http_rest_layer.py
+++ kb-classifier/http_rest_layer.py
@@ -4,7 +4,7 @@ from cachetools import cached, LFUCache
 from flask import Flask, request, jsonify
 import uuid
 
-from classifier import Classifier
+from embedding_algorithm import EmbeddingAlgorithm
 from kb_common import wiki_topics_to_actual_topics, topic_depth, dao_init, lookup_cache_init
 
 
@@ -17,10 +17,10 @@ doc_id_to_depth_prob_cache = LFUCache(maxsize=100000)
 doc_id_to_all_prob_cache = LFUCache(maxsize=100000)
 
 app = Flask(__name__)
-classifier = Classifier(dao=dao_init(),
-                        root_topic_names=wiki_topics_to_actual_topics.keys(),
-                        max_depth=topic_depth,
-                        phrase_cache=lookup_cache_init())
+embedder = EmbeddingAlgorithm(dao=dao_init(),
+                              root_topic_names=wiki_topics_to_actual_topics.keys(),
+                              max_depth=topic_depth,
+                              phrase_cache=lookup_cache_init())
 
 
 @app.route('/classify', methods=['POST'])
@@ -55,19 +55,19 @@ def probabilities(depth):
 
 @cached(doc_id_to_topic_prob_cache)
 def identify_topic_probabilities(doc_id):
-    topic_to_prob = classifier.identify_topic_probabilities(doc_id_to_text[doc_id])
+    topic_to_prob = embedder.identify_topic_probabilities(doc_id_to_text[doc_id])
     return jsonify(topic_to_prob)
 
 
 @cached(doc_id_to_depth_prob_cache)
 def get_topic_probabilities(doc_id, depth):
-    probabilities = classifier.get_topic_probabilities(depth)
+    probabilities = embedder.get_topic_probabilities(depth)
     return jsonify(probabilities)
 
 
 @cached(doc_id_to_all_prob_cache)
 def get_all_topic_probabilities(doc_id):
-    probabilities = classifier.get_all_topic_probabilities()
+    probabilities = embedder.get_all_topic_probabilities()
     return jsonify(probabilities)
     
 
