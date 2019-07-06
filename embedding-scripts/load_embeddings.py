7a0f30f81a5d767233bbe3006b666ac638c7db17
Daniel Cook <danielsteven.cook@gmail.com>
2019-07-06 12:36:36 +0100

    Changed to fast avro.

--- embedding-scripts/load_embeddings.py
+++ embedding-scripts/load_embeddings.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from avro.datafile import DataFileReader
-from avro.io import DatumReader
+from fastavro import reader
 
 
 def load_document_embeddings(path):
@@ -16,36 +15,34 @@ def load_document_embeddings(path):
     embedding_dimension = 0
     
     # First pass to work out maximum topic ID to create numpy embeddings
-    reader = DataFileReader(open(path, 'rb'), DatumReader())
-    for document_embedding in reader:
-        topic_probs = document_embedding['topic_probs']
-        
-        for topic_prob in topic_probs:
-            topic_id = topic_prob['topic_id']
-            if topic_id + 1 > embedding_dimension:
-                embedding_dimension = topic_id + 1
-    
-    reader.close()
+    with open(path, 'rb') as avro_file:
+        avro_reader = reader(avro_file)
+        for document_embedding in avro_reader:
+            topic_probs = document_embedding['topic_probs']
+            
+            for topic_prob in topic_probs:
+                topic_id = topic_prob['topic_id']
+                if topic_id + 1 > embedding_dimension:
+                    embedding_dimension = topic_id + 1
     
     # Second pass to actually store the embeddings
     x = []
     y = []
     
-    reader = DataFileReader(open(path, 'rb'), DatumReader())
-    for document_embedding in reader:
-        label = document_embedding['label']
-        topic_probs = document_embedding['topic_probs']
-        
-        embedding = np.zeros(shape=embedding_dimension)
-        
-        for topic_prob in topic_probs:
-            topic_id = topic_prob['topic_id']
-            prob = topic_prob['prob']
-            embedding[topic_id] = prob
-        
-        x.append(embedding)
-        y.append(label)
-    
-    reader.close()
+    with open(path, 'rb') as avro_file:
+        avro_reader = reader(avro_file)
+        for document_embedding in avro_reader:
+            label = document_embedding['label']
+            topic_probs = document_embedding['topic_probs']
+            
+            embedding = np.zeros(shape=embedding_dimension, dtype=np.float32)
+            
+            for topic_prob in topic_probs:
+                topic_id = topic_prob['topic_id']
+                prob = topic_prob['prob']
+                embedding[topic_id] = prob
+            
+            x.append(embedding)
+            y.append(label)
     
     return x, y
