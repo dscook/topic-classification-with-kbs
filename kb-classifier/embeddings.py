575e011a3c4a8b242aee186b36d7ba54bf2a29a8
Daniel Cook <danielsteven.cook@gmail.com>
2019-07-24 15:49:44 +0100

    Added further word embedding analysis.
    RCV1 results updated.
    
    UVigoMED results updated.

--- kb-classifier/embeddings.py
+++ kb-classifier/embeddings.py
@@ -21,13 +21,23 @@ class EmbeddingModel:
         self.topic_id_to_index = {}
         
         # Determine topics involved in the embeddings
+        self.topic_id_to_name = {}
+        self.topic_name_to_id = {}
         self.embedding_dim = 0
         with open(topic_id_mapping_path, newline='') as csvfile:
             mapping_reader = csv.reader(csvfile)
             for row in mapping_reader:
+                topic_name = row[0]
                 identifier = int(row[1])
                 self.topic_id_to_index[identifier] = self.embedding_dim
                 self.embedding_dim += 1
+                self.topic_id_to_name[identifier] = topic_name
+                self.topic_name_to_id[topic_name] = identifier
+        
+        # Maintain index to topic mapping
+        self.index_to_topic = {}
+        for topic_id, index in self.topic_id_to_index.items():
+            self.index_to_topic[index] = self.topic_id_to_name[topic_id]
         
         # To store phrase to embedding vector
         self.phrase_to_embedding = {}
@@ -49,7 +59,8 @@ class EmbeddingModel:
             self.phrase_to_embedding[phrase] = embedding
             
         reader.close()
-        
+
+
     def get_vector(self, word):
         """
         Get the word embedding for the given word.
@@ -58,9 +69,32 @@ class EmbeddingModel:
         :returns: a vector containing the word embedding.
         """
         return self.phrase_to_embedding[word]
-    
+
+
     def get_embedding_dim(self):
         """
         :returns: the length of the knowledge base word embeddings.
         """
         return self.embedding_dim
+
+
+    def get_topic_for_index(self, index):
+        """
+        Gets the topic name for a supplied embedding position index.  Gives a human understandable term that the
+        particular feature represents.
+        
+        :param index: the embedding position to obtain a topic name for.
+        :returns: the name of the topic that corresponds to the given embedding position.
+        """
+        return self.index_to_topic[index]
+
+
+    def get_index_for_topic(self, topic):
+        """
+        Gets the embedding index for a human readable topic name.
+        
+        :param topic: the name of the topic to obtain the embedding position for.
+        :returns: the embedding position that corresponds to the given topic.
+        """
+        topic_id = self.topic_name_to_id[topic]
+        return self.topic_id_to_index[topic_id]
