d09f934cde82d1e6b595488a8261bb3e724d7b9c
Daniel Cook <danielsteven.cook@gmail.com>
2019-07-27 13:48:13 +0100

    Code to ensure valid phrase matches are found.

--- experiment-runners/run_lstm_kb_phrase_embeddings.py
+++ experiment-runners/run_lstm_kb_phrase_embeddings.py
@@ -10,7 +10,7 @@ import numpy as np
 
 from loader import load_preprocessed_data
 from word_embeddings import DocToIntSequenceConverter
-from lstm_common import split_data, calculate_max_sequence_length
+from lstm_common import split_data, calculate_max_word_length
 from lstm_word import LstmPredictor
 from embeddings import EmbeddingModel
 from sklearn.metrics import classification_report
@@ -19,18 +19,34 @@ from sklearn.utils.class_weight import compute_class_weight
 ###
 ### VARIABLES (update as necessary)
 ###
-classification_problem_path = '../ohsumed/'
-train_data_path = '../ohsumed/data/ohsumed_lemmatized_train.csv'
-test_data_path = '../ohsumed/data/ohsumed_lemmatized_test.csv'
-phrase_embedding_path = '../ohsumed/embeddings/phrase_embeddings.avro'
-phrase_topic_id_mapping_path = '../ohsumed/embeddings/phrase_topic_id_mapping.csv'
+classification_problem_path = '../rcv1/'
+train_data_path = '../rcv1/data/rcv1_kb.csv'
+test_data_path = None
+phrase_embedding_path = '../rcv1/embeddings/phrase_embeddings.avro'
+phrase_topic_id_mapping_path = '../rcv1/embeddings/phrase_topic_id_mapping.csv'
 
 
 ###
 ### CLASSIFICATION PROBLEM SPECIFIC IMPORTS
 ###
 sys.path.append(classification_problem_path)
-from lookup_tables import int_to_topic
+
+if classification_problem_path == '../rcv1/':
+    from lookup_tables import int_to_topic_code
+    int_to_topic = int_to_topic_code
+else:
+    from lookup_tables import int_to_topic
+
+
+###
+### GET THE CUSTOM KNOWLEDGE BASE WORD EMBEDDINGS
+###
+embedding_model = EmbeddingModel(phrase_embedding_path,
+                                 phrase_topic_id_mapping_path,
+                                 underscored_phrase=True)
+word_embedding_dim = embedding_model.get_embedding_dim()
+print('Word embedding dimension is {}'.format(word_embedding_dim))
+
 
 ###
 ### LOAD THE DATA
@@ -56,38 +72,98 @@ y = np.array(y)
 train_x, train_y, val_x, val_y, test_x, test_y = split_data(x, y)
 print('Number of training examples: {}'.format(len(train_x)))
 
-max_sequence_length = calculate_max_sequence_length(train_x)
 
-# Convert articles to sequence of integers representing the words
+###
+### OBTAIN THE PHRASES
+###
+
+def convert_to_valid_phrases(x):
+    """
+    Group separate words into phrases where appropriate by examining the list of valid phrase embeddings.
+    
+    :param x: list of documents as strings.
+    :returns: the list of documents with valid phrases joined by an underscore and non valid phrases removed.
+    """
+    updated_x = []
+    
+    for doc in x:
+        tokens = doc.split()
+        phrase_tokens = []
+        
+        # Maintain the start of the phrase we are processing
+        index = 0
+        
+        # Initially consider a phrase of word length 3
+        phrase_length = 3
+        
+        while index < len(tokens):
+            
+            # Check the phrase length doesn't exceed the end of the string
+            while index + phrase_length > len(tokens):
+                phrase_length -= 1
+            
+            # Try and find a match for 3 word phrase, then 2 then 1
+            while phrase_length > 0:
+                # Check to see if we have a valid phrase match
+                updated_tokens = []
+                for token in tokens[index:index+phrase_length]:
+                    updated_tokens.extend(token.split('_'))
+                
+                phrase = '_'.join(updated_tokens)
+                
+                try:
+                    embedding_model.get_vector(phrase)
+                    print('Matched {}'.format(phrase))
+                    phrase_tokens.append(phrase)
+                    break
+                except:
+                    phrase_length -= 1
+            
+            # Cover the case where we couldn't find a valid phrase
+            if phrase_length == 0:
+                phrase_length = 1
+                
+            # We have a match, consume the phrase
+            index += phrase_length
+            
+            # Reset phrase length for processing next index
+            phrase_length = 3
+        
+        print(' '.join(phrase_tokens))
+        updated_x.append(' '.join(phrase_tokens))
+    
+    return updated_x
+
+
+train_x = convert_to_valid_phrases(train_x)
+val_x = convert_to_valid_phrases(val_x)
+test_x = convert_to_valid_phrases(test_x)
+
+max_sequence_length = calculate_max_word_length(train_x)
+
+# Convert articles to sequence of integers representing the phrases
 article_to_int_seq_converter = DocToIntSequenceConverter(x, max_sequence_length)
 train_x_seq = article_to_int_seq_converter.convert_to_integer_sequences(train_x)
 val_x_seq = article_to_int_seq_converter.convert_to_integer_sequences(val_x)
 test_x_seq = article_to_int_seq_converter.convert_to_integer_sequences(test_x)
 
-###
-### GET THE CUSTOM KNOWLEDGE BASE WORD EMBEDDINGS
-###
-embedding_model = EmbeddingModel(phrase_embedding_path, phrase_topic_id_mapping_path)
-word_embedding_dim = embedding_model.get_embedding_dim()
-print('Word embedding dimension is {}'.format(word_embedding_dim))
-
 
 ###
 ### TRAIN THE LSTM
 ###
-lstm = LstmPredictor(article_to_int_seq_converter.get_word_index(),
-                     word_embedding_dim,
-                     max_sequence_length,
-                     embedding_model,
-                     len(int_to_topic.values()),
-                     weights_path='models/lstm_kb_embeddings.h5')
+#lstm = LstmPredictor(article_to_int_seq_converter.get_word_index(),
+#                     word_embedding_dim,
+#                     max_sequence_length,
+#                     embedding_model,
+#                     len(int_to_topic.values()),
+#                     weights_path='models/lstm_kb_embeddings.h5')
 
 
-class_weights = compute_class_weight('balanced', np.unique(train_y), train_y)
-class_weights_dict = {}
-for i in range(len(class_weights)):
-    class_weights_dict[i] = class_weights[i]
-lstm.train(train_x_seq, train_y, val_x_seq, val_y, class_weights_dict)
+#class_weights = compute_class_weight('balanced', np.unique(train_y), train_y)
+#class_weights_dict = {}
+#for i in range(len(class_weights)):
+#    class_weights_dict[i] = class_weights[i]
+#lstm.train(train_x_seq, train_y, val_x_seq, val_y, class_weights_dict)
 
 
 ###
@@ -95,12 +171,12 @@ lstm.train(train_x_seq, train_y, val_x_seq, val_y, class_weights_dict)
 ###
 
 # Re-initialise the LSTM, will use weights from the previous training run.
-lstm = LstmPredictor(article_to_int_seq_converter.get_word_index(),
-                     word_embedding_dim,
-                     max_sequence_length,
-                     embedding_model,
-                     len(int_to_topic.values()),
-                     use_saved_weights=True,
-                     weights_path='models/lstm_kb_embeddings.h5')
-test_y_predict = lstm.predict(test_x_seq)
-print(classification_report(test_y, test_y_predict, digits=6, target_names=int_to_topic.values()))
+#lstm = LstmPredictor(article_to_int_seq_converter.get_word_index(),
+#                     word_embedding_dim,
+#                     max_sequence_length,
+#                     embedding_model,
+#                     len(int_to_topic.values()),
+#                     use_saved_weights=True,
+#                     weights_path='models/lstm_kb_embeddings.h5')
+#test_y_predict = lstm.predict(test_x_seq)
+#print(classification_report(test_y, test_y_predict, digits=6, target_names=int_to_topic.values()))
