437901ab52c23852d35c6cbadc90a00e80897370
Daniel Cook <danielsteven.cook@gmail.com>
2019-07-29 07:36:33 +0100

    Code to run pre-trained embeddings.

--- experiment-runners/run_lstm_phrase_embeddings.py
+++ experiment-runners/run_lstm_phrase_embeddings.py
@@ -20,12 +20,23 @@ from lstm_word import LstmPredictor
 ###
 ### VARIABLES (update as necessary)
 ###
-classification_problem_path = '../uvigomed/'
-train_data_path = '../uvigomed/data/uvigomed_train.csv'
-test_data_path = '../uvigomed/data/uvigomed_test.csv'
+classification_problem_path = '../rcv1/'
+train_data_path = '../rcv1/data/rcv1_kb.csv'
+test_data_path = None
 
 use_word2vec = True    # Set to False to use GloVe embeddings
 
+###
+### CLASSIFICATION PROBLEM SPECIFIC IMPORTS
+###
+sys.path.append(classification_problem_path)
+
+if classification_problem_path == '../rcv1/':
+    from lookup_tables import int_to_topic_code
+    int_to_topic = int_to_topic_code
+else:
+    from lookup_tables import int_to_topic
+    
 
 ###
 ### GET THE WORD EMBEDDINGS
@@ -83,3 +94,37 @@ article_to_int_seq_converter = DocToIntSequenceConverter(x, max_sequence_length)
 train_x_seq = article_to_int_seq_converter.convert_to_integer_sequences(train_x)
 val_x_seq = article_to_int_seq_converter.convert_to_integer_sequences(val_x)
 test_x_seq = article_to_int_seq_converter.convert_to_integer_sequences(test_x)
+
+
+###
+### TRAIN THE LSTM
+###
+lstm = LstmPredictor(article_to_int_seq_converter.get_word_index(),
+                     word_embedding_dim,
+                     max_sequence_length,
+                     embedding_model,
+                     len(int_to_topic.values()),
+                     weights_path='models/lstm_kb_embeddings.h5')
+
+
+class_weights = compute_class_weight('balanced', np.unique(train_y), train_y)
+class_weights_dict = {}
+for i in range(len(class_weights)):
+    class_weights_dict[i] = class_weights[i]
+lstm.train(train_x_seq, train_y, val_x_seq, val_y, class_weights_dict)
+
+
+###
+### MAKE THE PREDICTIONS
+###
+
+# Re-initialise the LSTM, will use weights from the previous training run.
+lstm = LstmPredictor(article_to_int_seq_converter.get_word_index(),
+                     word_embedding_dim,
+                     max_sequence_length,
+                     embedding_model,
+                     len(int_to_topic.values()),
+                     use_saved_weights=True,
+                     weights_path='models/lstm_kb_embeddings.h5')
+test_y_predict = lstm.predict(test_x_seq)
+print(classification_report(test_y, test_y_predict, digits=6, target_names=int_to_topic.values()))
